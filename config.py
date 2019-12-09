d456311ebc90264d62a59a4c52c2106c4cb7fec5
cyberfrentic <j_hugo_p_c3@msn.com>
2019-12-09 13:44:44 -0600

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 
 class Config(object):
-    SECRET_KEY = '7q%3=;8J+X5:f.+pU9e!;6x:E*n_9^Ky0~.R'
+    SECRET_KEY = '7897q%3=;8J+X5:f.+pU9e!;6x:E*n_9^Ky0~.R465'
 
 ########################################################################################
 # CONEXION A MYSQL
